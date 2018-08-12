95b4ed3778c66c1c40dbe693ca51b65796d77c1e
roedoejet <aidanpine@shaw.ca>
2018-08-12 11:39:00 +0900

    added flask freeze to fab

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,11 @@
 from latex import LatexTemplate
+from flask_frozen import Freezer
+from cv import app
 
 def latex():
     ltx = LatexTemplate()
     ltx.export()
+
+def freeze():
+    freezer = Freezer(app)
+    freezer.freeze()
