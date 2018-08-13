2e3ddeb94d2a5d258c63bdb6312b646ee92324be
roedoejet <aidanpine@shaw.ca>
2018-08-13 12:16:22 +0900

    added docs folder

--- fabfile.py
+++ fabfile.py
@@ -1,12 +1,15 @@
 from latex import LatexTemplate
 from flask_frozen import Freezer
 from cv import app
+from fabric.api import local
 
 def latex():
     ltx = LatexTemplate()
     ltx.export()
 
 def freeze():
-    app.config['FREEZER_BASE_URL'] = 'https://roedoejet.github.io/cv-flask/cv/build/'
+    app.config['FREEZER_BASE_URL'] = 'https://roedoejet.github.io/cv-flask/'
     freezer = Freezer(app)
     freezer.freeze()
+    local('rm -r docs/*')
+    local('cp -r cv/build/* docs/')
