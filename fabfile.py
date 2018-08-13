199d7f70bc5289b07fb99dceaace829ad919243d
roedoejet <aidanpine@shaw.ca>
2018-08-13 12:50:42 +0900

    updated base url

--- fabfile.py
+++ fabfile.py
@@ -8,7 +8,7 @@ def latex():
     ltx.export()
 
 def freeze():
-    app.config['FREEZER_BASE_URL'] = 'https://roedoejet.github.io/cv-flask/'
+    app.config['FREEZER_BASE_URL'] = 'https://roedoejet.github.io/cv/'
     freezer = Freezer(app)
     freezer.freeze()
     local('rm -r docs/*')
