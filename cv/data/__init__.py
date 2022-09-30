f651b8ce4f0e227552199f7bbbdb101a095e7a89
roedoejet <aidanpine@shaw.ca>
2022-09-29 18:40:30 -0700

    updated cv

--- cv/data/__init__.py
+++ cv/data/__init__.py
@@ -2,7 +2,7 @@ import os
 import yaml
 import cv.data as data
 
-DATAPATH = os.path.join(os.path.dirname(data.__file__), 'cv.yml')
+DATAPATH = os.path.join(os.path.dirname(data.__file__), "cv.yml")
 
-with open(DATAPATH, 'r', encoding='utf8') as yml:
+with open(DATAPATH, "r", encoding="utf8") as yml:
     DATA = yaml.load(yml)
