ebfab4cb017f0ca3abb399d867ce110056cf962d
roedoejet <hello@aidanpine.ca>
2023-03-06 22:49:57 -0800

    chore: update site and use safe_load

--- cv/data/__init__.py
+++ cv/data/__init__.py
@@ -5,4 +5,4 @@ import cv.data as data
 DATAPATH = os.path.join(os.path.dirname(data.__file__), "cv.yml")
 
 with open(DATAPATH, "r", encoding="utf8") as yml:
-    DATA = yaml.load(yml)
+    DATA = yaml.safe_load(yml)
