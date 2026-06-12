e90a97947be397ae7077ffb83ae6239a0b4ec68c
Aidan Pine <hello@aidanpine.ca>
2026-06-11 17:55:57 -0700

    bust old css versions

--- cv/__init__.py
+++ cv/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import hashlib
 import os
 from slugify import slugify
 
@@ -24,5 +25,21 @@ def alph(n):
 @app.template_filter('slug')
 def slug(s):
     return slugify(s)
+
+def _static_hash(filename):
+    """Return an 8-char MD5 of a static file for cache-busting."""
+    path = os.path.join(app.static_folder, filename)
+    try:
+        with open(path, 'rb') as f:
+            return hashlib.md5(f.read()).hexdigest()[:8]
+    except OSError:
+        return '0'
+
+@app.context_processor
+def inject_asset_versions():
+    return {
+        'css_v': _static_hash('cv/dist/css/custom.css'),
+        'js_v':  _static_hash('cv/dist/js/main.js'),
+    }
 
 import cv.views
